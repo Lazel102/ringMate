a425906ba511746842db1c5428d56813228335b8
Raphaël Oculi <raphael.oculi@etu.minesparis.psl.eu>
2024-11-28 11:40:35 +0100

    new

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,8 +19,8 @@ eps_z=25
 
 i=0
 N=len(L)
-dic_value_x_begin={0 : L[0][coordinates][x]}
-dic_value_y_begin={L[0][coordinates][y]}
+dic_value_x_begin={0 : L[0]["coordinates"]["x"]}
+dic_value_y_begin={L[0]["coordinates"]["y"]}
 value_x_begin=0
 value_y_begin=0
 value_y_end=0
@@ -28,20 +28,20 @@ value_x_end=0
 dic_value_x_end={}
 dic_value_y_end={}
 
-while np.abs(L[i][coordinates][x]-L[0][coordinates][x])<eps_z :
-    dic_value_x_begin[i+1]=L[i+1][coordinates][x]
-    dic_value_y_begin[i+1]=L[i+1][coordinates][y]
+while np.abs(L[i]["coordinates"]["x"]-L[0]["coordinates"]["x"])<eps_z :
+    dic_value_x_begin[i+1]=L[i+1]["coordinates"]["x"]
+    dic_value_y_begin[i+1]=L[i+1]["coordinates"]["y"]
     i=i+1
 for j in range(len(dic_value_x_begin)):
     value_x_begin+=dic_value_x_begin[j]
     value_y_begin+=dic_value_y_begin[j]
 value_x_begin=value_x_begin/len(dic_value_x_begin)
 value_y_begin=value_y_begin/len(dic_value_x_begin)
-while np.abs(L[i][coordinates][x]-L[0][coordinates][x])>eps_z :
+while np.abs(L[i]["coordinates"]["x"]-L[0]["coordinates"]["x"])>eps_z :
     i=i+1
 while i<len(L):
-    dic_value_x_end[i+1]=L[i+1][coordinates][x]
-    dic_value_y_end[i+1]=L[i+1][coordinates][y]
+    dic_value_x_end[i+1]=L[i+1]["coordinates"]["x"]
+    dic_value_y_end[i+1]=L[i+1]["coordinates"]["y"]
     i=i+1
 for j in range(len(dic_value_x_end)):
     value_x_end+=dic_value_x_end[j]
